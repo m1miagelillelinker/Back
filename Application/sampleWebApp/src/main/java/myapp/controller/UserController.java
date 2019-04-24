047dbcc78c9fdb02119654101698788ebe9c5a1a
Mathieu <msansen@gmail.com>
2019-04-24 21:55:49 +0200

    implementation de spring security en cours mais non finalisé
    unauthorised pour le moment
    
    /user passé en POST

--- Application/sampleWebApp/src/main/java/myapp/controller/UserController.java
+++ Application/sampleWebApp/src/main/java/myapp/controller/UserController.java
@@ -9,8 +9,13 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping(value = "/user" , produces = MediaType.APPLICATION_JSON_VALUE )
+//modifié pour répondre aux query en POST du type
+// curl -i -d userId=2 http://localhost:8080/user
+
+//l'objectif actuel est de répondre a l'appel
+// curl -i -d userId=2 -d username=user -d password=userPass http://localhost:8080/user sans recevoir un "status":401,"error":"Unauthorized"
+
+@RestController
 public class UserController {
 
     private final UserBusiness userBusiness;
@@ -24,9 +29,9 @@ public class UserController {
     }
 
     @CrossOrigin
-    @GetMapping("/get")
+    @RequestMapping(value = "/user" , produces = MediaType.APPLICATION_JSON_VALUE )
     @ResponseBody
-    public User getUserById(@RequestParam("userId") String userId){
+    public User user(@RequestParam(value="userId", defaultValue="1") String userId){
         System.out.println(userId);
 
         User user = userService.getUserById(Integer.parseInt(userId));
