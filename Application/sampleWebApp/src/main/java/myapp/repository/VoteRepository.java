a3b066e353801a733823a73358795f4b31cd5856
Mitrail <edouardguegain@gmail.com>
2019-04-28 01:09:05 +0200

    WIP : CRUD votes

--- Application/sampleWebApp/src/main/java/myapp/repository/VoteRepository.java
+++ Application/sampleWebApp/src/main/java/myapp/repository/VoteRepository.java
@@ -1,9 +1,17 @@
 package myapp.repository;
 
 import myapp.model.Vote;
+
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VoteRepository extends CrudRepository<Vote,Integer> {
+
+	Optional<List<Vote>> findAllByIdUser(String userId);
+
+	Optional<List<Vote>> findAllByIdAssoc(String associationId);
+
 }
--- Application/sampleWebApp/src/main/java/myapp/service/VoteService.java
+++ Application/sampleWebApp/src/main/java/myapp/service/VoteService.java
@@ -1,4 +1,16 @@
 package myapp.service;
 
+import java.util.List;
+
+import myapp.model.Vote;
+
 public interface VoteService {
+	
+	Vote upsertVote(Vote vote);
+	
+	void deleteVote(Vote vote);
+	
+	List<Vote> getVotesByUser(String userId);
+	
+	List<Vote> getVotesByAssociation(String associationId);
 }
