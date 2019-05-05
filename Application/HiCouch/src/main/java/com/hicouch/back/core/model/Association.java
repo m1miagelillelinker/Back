924c9b3367776ef105898f884a98ea674adc75ef
Mitrail <edouardguegain@gmail.com>
2019-05-05 20:14:05 +0200

    fix : nommage endpoints & typo

--- Application/HiCouch/src/main/java/com/hicouch/back/core/model/Association.java
+++ Application/HiCouch/src/main/java/com/hicouch/back/core/model/Association.java
@@ -8,7 +8,7 @@ import java.util.Date;
 public class Association {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id")
     Integer id;
 
