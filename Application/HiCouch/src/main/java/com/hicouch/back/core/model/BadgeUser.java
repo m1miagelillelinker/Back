ade5cd19695eb6dce3ee56858ae837162d6d606b
mathieu <msansen@gmail.com>
2019-06-07 12:12:13 +0200

    WIP audit

--- Application/HiCouch/src/main/java/com/hicouch/back/core/model/BadgeUser.java
+++ Application/HiCouch/src/main/java/com/hicouch/back/core/model/BadgeUser.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
 public class BadgeUser {
 	
 	@Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column
 	int id;
 	
