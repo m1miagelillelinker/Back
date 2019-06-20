d726f29c29c636c2299aa45ce946d109c8910494
Mitrail <edouardguegain@gmail.com>
2019-06-20 11:30:46 +0200

    WIP : historique DTO factory

--- Application/HiCouch/src/main/java/com/hicouch/back/core/factory/HistoriqueFactory.java
+++ Application/HiCouch/src/main/java/com/hicouch/back/core/factory/HistoriqueFactory.java
@@ -7,8 +7,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.hicouch.back.core.dto.AssociationDTO;
 import com.hicouch.back.core.dto.CommentaireDTO;
 import com.hicouch.back.core.dto.HistoriqueDTO;
+import com.hicouch.back.core.enumeration.HistoriqueTypeEnum;
 import com.hicouch.back.core.exception.BusinessException;
 import com.hicouch.back.core.exception.NoResultException;
 import com.hicouch.back.core.model.Commentaire;
@@ -20,13 +22,15 @@ public class HistoriqueFactory {
 	
 	private UserService userService;
     private AssociationService associationService;
+    private AssociationFactory associationFactory;
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
-    public HistoriqueFactory(UserService userService, AssociationService associationService) {
+    public HistoriqueFactory(UserService userService, AssociationService associationService, AssociationFactory associationFactory) {
         super();
         this.userService        = userService;
         this.associationService = associationService;
+        this.associationFactory = associationFactory;
     }
 	
 	public HistoriqueDTO getHistoriqueDTO(Historique historique) {
@@ -42,7 +46,28 @@ public class HistoriqueFactory {
         	e.printStackTrace();
         }
         
-        
+        switch (historique.getType()) {
+		case HistoriqueTypeEnum.ABONNEMENT:
+			historiqueDTO.setFollowId(historique.getIdFollow());
+			try {
+	        	historiqueDTO.setFollowUserName(userService.getUserById(historique.getIdFollow()).getPseudo());
+	        } catch (BusinessException e) {
+	        	logger.error("No result for user " + historique.getIdFollow());
+	        	e.printStackTrace();
+	        }
+			break;
+		case HistoriqueTypeEnum.ASSOCIATION:
+		case HistoriqueTypeEnum.COMMENTAIRE:
+			AssociationDTO associationDTO = associationService.getAssociationByIdPair(historique.getIdAsso());
+			historiqueDTO.setIdProduitA(associationDTO.getProductA().getId());
+			historiqueDTO.setIdProduitB(associationDTO.getProductB().getId());
+			historiqueDTO.setIdTypeProduitA(associationDTO.getProductA().getType());
+			historiqueDTO.setIdTypeProduitB(associationDTO.getProductB().getType());
+			historiqueDTO.setNomProduitA(associationDTO.getProductA().getTitle());
+			historiqueDTO.setNomProduitB(associationDTO.getProductB().getTitle());
+		default:
+			break;
+		}
         
         return historiqueDTO;
    
