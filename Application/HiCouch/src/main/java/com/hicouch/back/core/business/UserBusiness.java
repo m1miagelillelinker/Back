10a574e5711c7a01c7f58683fd1dd0bd387967e7
tchobels <alexis.bels@outlook.fr>
2019-06-26 21:17:02 +0200

    WIP: fix en cours

--- Application/HiCouch/src/main/java/com/hicouch/back/core/business/UserBusiness.java
+++ Application/HiCouch/src/main/java/com/hicouch/back/core/business/UserBusiness.java
@@ -1,4 +1,8 @@
 package com.hicouch.back.core.business;
 
+import com.hicouch.back.core.exception.NoResultException;
+import com.hicouch.back.core.model.User;
+
 public interface UserBusiness {
+    User getUserById(int parseInt) throws NoResultException;
 }
